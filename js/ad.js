d175fd8fa7e658126fd6b3958ec8132c5ab5aa63
junhuazhou <zhou-junhua@hotmail.com>
2016-11-30 23:07:16 +0800

    Add files via upload

---
+++ js/ad.js
@@ -0,0 +1,58 @@
+$(function () {
+    var index = 0;
+    var inverval = 1500;
+    var timer;
+    //轮播区域
+    $("#next").click(function () {
+        index ++;
+        if (index ==4){
+            $(".ad").find("img").eq(index).fadeIn()
+                .siblings().fadeOut();
+            index = -1;
+        }
+        else {
+            $(".ad").find("img").eq(index).fadeIn()
+                .siblings().fadeOut();
+        }
+        showButtons();
+    })
+    $("#prev").click(function () {
+        index --;
+        if (index ==-1){
+            $(".ad").find("img").eq(index+5).fadeIn()
+                .siblings().fadeOut();
+            index = 4;
+        }
+        else {
+            $(".ad").find("img").eq(index).fadeIn()
+                .siblings().fadeOut();
+        }
+        showButtons();
+    })
+    $("#num span").click(function () {
+        index = parseInt($(this).attr('index'));
+        $(".ad").find("img").eq(index).fadeIn()
+            .siblings().fadeOut();
+        showButtons();
+    })
+    play();
+    $('.container').hover(stop,play);
+
+    //左侧目录
+    $("#shut_down").click(function () {
+        $("header").slideUp();
+    })
+    //封装函数
+    function play() {
+        timer = setTimeout(function () {
+            $("#next").trigger("click");
+            play();
+        },inverval);
+    }
+    function showButtons() {
+        $("#num span").eq(index).addClass("on").siblings().removeClass();
+    }
+    function stop() {
+        clearTimeout(timer);
+    }
+})
